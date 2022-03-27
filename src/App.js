73eb4f98fda889ec06e2349df7b300724f5ce186
Rami Theeb <rami.theeb00@gmail.com>
2022-03-27 12:01:38 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { WelcomeMessage } from "./WelcomeMessage"
+export const App = () => {
+    return <div>
+        <WelcomeMessage name="saeed" />
+        <WelcomeMessage name="saeed2" />
+
+        <WelcomeMessage name="saeed3" />
+        <WelcomeMessage name="saeed4" />
+        <WelcomeMessage name="saeed5" />
     </div>
-  );
+
 }
 
-export default App;
---
+++ src/WelcomeMessage.js
@@ -0,0 +1,4 @@
+
+export const WelcomeMessage = (props) => {
+    return <h1> {props.name} ,welcome to our site !!</h1>
+}
