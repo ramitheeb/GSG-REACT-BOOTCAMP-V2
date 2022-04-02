5b31a90acc5d5c722178d94560aa6b1c6dcfd5d9
Rami Theeb <rami.theeb00@gmail.com>
2022-04-02 10:39:23 +0300

    missing code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 
 import { WelcomeMessage } from "./WelcomeMessage"
 import { Post } from "./Post"
-
 import "./styles.css"
 import { Counter } from "./Counter"
-const posts = [
-    {
-        title: "HELOOOOOOOOOOOOOOOOO",
-        imageSrc: "https://images.pexels.com/photos/11341064/pexels-photo-11341064.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        "
-    },
-    {
-        title: "title 1",
-        imageSrc: "https://images.pexels.com/photos/11341064/pexels-photo-11341064.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        "
-    },
-    {
-        title: "title 2",
-        imageSrc: "https://images.pexels.com/photos/11341064/pexels-photo-11341064.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        "
-    }, {
-        title: "title 3",
-        imageSrc: "https://images.pexels.com/photos/11341064/pexels-photo-11341064.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        "
-    },
-]
+import { useEffect, useState } from "react"
+
+
 
 
 export const App = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => {
+                setPosts(json)
+            });
+    }, [])
+
     return <div className="container">
 
         {posts.map(post => <Post title={post.title} imageSrc={post.imageSrc} body={post.body} />)}
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const Post = (props) => {
 
@@ -8,13 +8,15 @@ export const Post = (props) => {
     }
 
 
+
     return <div className="post">
 
         <h1>{props.title}</h1>
-        <img src={props.imageSrc} className="image" />
+        <img src="https://images.pexels.com/photos/11341064/pexels-photo-11341064.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" className="image" />
         <p>{props.body}</p>
         <button onClick={handleLikeClick}>Like</button>
         <span style={{ marginLeft: 20 }}>{likes}</span>
+        {/* <input value={comments} onChange={(e) => setComments(e.target.value)} /> */}
 
     </div>
 }
