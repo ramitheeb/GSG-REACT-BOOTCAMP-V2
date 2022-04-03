f200d81fbc7a3d62035b78a90443c1b25676a111
Rami Theeb <rami.theeb00@gmail.com>
2022-04-03 12:59:21 +0300

    add header functionality

--- src/components/ToDoHeader.js
+++ src/components/ToDoHeader.js
@@ -1,16 +1,28 @@
 import { useState } from "react"
 
 
-const ToDoHeader = () => {
+const ToDoHeader = (props) => {
 
     const [title, setTitle] = useState("")
     const handleTitleChange = (e) => {
         setTitle(e.target.value)
     }
 
+    const handleAddToDo = () => {
+        if (title.length == 0) return
+        const newToDo = {
+            title: title,
+            id: Math.random(),
+            date: new Date()
+        }
+        props.onToDoAdd(newToDo)
+        setTitle("")
+
+    }
+
     return <div>
         <input type="text" placeholder="title" value={title} onChange={handleTitleChange} />
-        <button> +</button>
+        <button onClick={handleAddToDo} > +</button>
 
     </div>
 }
--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,33 +1,19 @@
+import { useState } from "react"
 import ToDoFooter from "../components/ToDoFooter"
 import ToDoHeader from "../components/ToDoHeader"
 import ToDoItem from "../components/ToDoItem"
-let toDos = [
-    {
-        title: "bbla aldflsa fsad ",
-        id: 1,
-        date: new Date(),
-
-    },
-    {
-        title: "bbla aldflsa fsad 2 ",
-        id: 2,
-        date: new Date(),
-
-    },
-    {
-        title: "bbla aldflsa fsad 3 ",
-        id: 3,
-        date: new Date(),
-
-    }
-]
-
 
 
 const ToDo = () => {
+    const [toDos, setToDos] = useState([])
+
+    const onToDoAdd = (toDo) => {
+        setToDos([toDo, ...toDos])
+    }
+
     return <div>
         <h1>To Do App </h1>
-        <ToDoHeader />
+        <ToDoHeader onToDoAdd={onToDoAdd} />
         {toDos.map(toDo => <ToDoItem title={toDo.title} />)}
         <ToDoFooter />
     </div>
